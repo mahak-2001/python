e8c1b9a9683eea58d54d89a86fa700bbd8889eb9
mahak-2001 <mahaktanwar171@gmail.com>
2026-06-29 23:45:36 +0530

    Update tupple.py

--- tupple.py
+++ tupple.py
@@ -1,26 +1,82 @@
-#tuples:createimmutable sequence of values.
-tup=(1,2,3,4)
-print(type(tup))
-tupl=(1,)#if we cannot use comma so it count as integer in python
-print(tupl)
+# #tuples:create immutable sequence of values.
+# tup=(1,2,3,4)
+# print(type(tup))
+# tupl=(1,)#if we cannot use comma so it count as integer in python
+# print(tupl)
 
-#METHODS:
+# #METHODS:
 tup=(1,2,3,4,2,2)
-print(tup.index(2))
+print(tup.index(1))
 print(tup.count(2))
 
-#test
-movies=[]
-mov1=input("enter movie: ")
-mov2=input("enter movie: ")
-mov3=input("enter movie: ")
-movies.append(mov1)
-movies.append(mov2)
-movies.append(mov3)
-print(movies)
-#another method:
-fruit=[]
-fruit.append(input("enter 1st fruit: "))
-fruit.append(input("enter 2nd fruit: "))
-fruit.append(input("enter 3rd fruit: "))
-print(fruit)
+# #test
+# movies=[]
+# mov1=input("enter movie: ")
+# mov2=input("enter movie: ")
+# mov3=input("enter movie: ")
+# movies.append(mov1)
+# movies.append(mov2)
+# movies.append(mov3)
+# print(movies)
+# #another method:
+# fruit=[]
+# fruit.append(input("enter 1st fruit: "))
+# fruit.append(input("enter 2nd fruit: "))
+# fruit.append(input("enter 3rd fruit: "))
+# print(fruit)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+t=("mahak",24,"python")
+x=list(t)
+print(x)
+#x.insert(1,"m")
+print(x.append(11))
+t=tuple(x)
+print(t)
+
+
+
+
+
+z=[1,2,[5,6,7,8,],9,3,]
+print(len(z))
+
+
+
+
+
+
+
+mix=("mahak",3,15.5,[1,2,3],True,3)
+print(mix)
+a=list(mix)
+a.pop()
+print(a)
+mix=tuple(a)
+print(mix)
+y=2*mix
+print(y)
+num=(1,)
+print(type(num))
+a=(1,2,3,4)
+b=(5,6,7,8)
+c=a+b
+print(c)
+number=((1,2),(3,4),5,6)
+#we use * for assign remaining value to single variable
+n1,n2,*n3=number
+print(n3)
+print(n1)
