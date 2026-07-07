006e6c0a457b246b00dc5233141a843f351a4e4c
mahak-2001 <mahaktanwar171@gmail.com>
2026-07-08 00:08:01 +0530

    some updates

--- test.py
+++ test.py
@@ -1,15 +1,22 @@
-# python={"mahak","jiya","twinkle","jaanvi"}
-# sql={"preeti","deepika","mahak","rinku"}
-# print(python.intersection(sql))
-person={
-    "person_1":{
-        "name":"mahak",
-        "course":"BCA"
-    },
-    "person_2":{
-        "name":"ram",
-        "course":"BBA"
-    }
-}
-print(person["person_1"]["name"])
-print(person["person_2"].get("email","not found"))
+# # python={"mahak","jiya","twinkle","jaanvi"}
+# # sql={"preeti","deepika","mahak","rinku"}
+# # print(python.intersection(sql))
+# person={
+#     "person_1":{
+#         "name":"mahak",
+#         "course":"BCA"
+#     },
+#     "person_2":{
+#         "name":"ram",
+#         "course":"BBA"
+#     }
+# }
+# print(person["person_1"]["name"])
+# print(person["person_2"].get("email","not found"))
+
+
+a=10
+b=40
+c=80
+a,b,c=b,a,c
+print(a,b,c)
