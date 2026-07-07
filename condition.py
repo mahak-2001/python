e61fb3b273e23ab63d9465f657db8c01427ff569
mahak-2001 <mahaktanwar171@gmail.com>
2026-07-07 14:07:21 +0530

    some changes

---
+++ condition.py
@@ -0,0 +1,15 @@
+age=18
+if age>=18:
+    print("you are adult.")
+else:
+    print("you are not adult.")
+
+marks=77
+if(marks>=60):
+    if (marks>=80):
+     print("good grades")
+    else:
+       print("okay grades")
+else:
+     print("grade is very low")
+
