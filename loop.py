350c9a8c9efdf64913bb9a182df2c20bb4ed4e41
mahak-2001 <mahaktanwar171@gmail.com>
2026-07-09 13:11:58 +0530

    loop program

--- loop.py
+++ loop.py
@@ -1,60 +1,60 @@
-# #loops:excute a block of code repeatedly until a condition become false.
-# #for loop
-# for i in range(0,20,3):
-#     print(i)
+#loops:excute a block of code repeatedly until a condition become false.
+#for loop
+for i in range(0,20,3):
+    print(i)
 
-# names=["mahak","jiya","teena","jaanvi"]
-# for name in names:
-#     print(name)
-# #range
-# for x in range(5):
-#     print(x)
+names=["mahak","jiya","teena","jaanvi"]
+for name in names:
+    print(name)
+#range
+for x in range(5):
+    print(x)
 
-# words="mahak"
-# for x in words:
-#     print(x)
+words="mahak"
+for x in words:
+    print(x)
 
-# #while loop
-# count = 0
-# while count<5:
-#     print(count)
-#     count +=1
+#while loop
+count = 0
+while count<5:
+    print(count)
+    count +=1
 
 
-# password=""
-# while password.strip() !="secret":
-#     password=input("enter your password: ")
+password=""
+while password.strip() !="secret":
+    password=input("enter your password: ")
     
-# print("access granted")
+print("access granted")
     
-#sum of 1,20 no. using for loop.
-# sum = 0
-# for i in range(1,21):
-#     sum=sum+i
-#     print(sum)
+sum of 1,20 no. using for loop.
+sum = 0
+for i in range(1,21):
+    sum=sum+i
+    print(sum)
 
-# #break and continue.
-# for i in range(1,6):
-#     if i==3:
-#         break
-#     print(i)
+#break and continue.
+for i in range(1,6):
+    if i==3:
+        break
+    print(i)
 
-# total=0
-# number=int(input("enter a number: "))
+total=0
+number=int(input("enter a number: "))
 
-# while number!=0:
-#     total+=number
-#     number=int(input("enter another number: "))
-# print("total",total)
+while number!=0:
+    total+=number
+    number=int(input("enter another number: "))
+print("total",total)
 
-# secret_number=7
-# guess=0
-# while guess !=secret_number:
-#     guess=int(input("guess the number:"))
-# print("correct number!")
+secret_number=7
+guess=0
+while guess !=secret_number:
+    guess=int(input("guess the number:"))
+print("correct number!")
 
 number=[1,2,3,4,5]
 for i in number:
-    if number == 3:
-        break
+    if i %2 == 0:
+        continue
     print(i)
