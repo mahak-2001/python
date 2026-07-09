2f7ab8fe8ac09260c530ce42e5e4e6cbc396d3d2
mahak-2001 <mahaktanwar171@gmail.com>
2026-07-09 14:04:06 +0530

    function method
    function

---
+++ function.py
@@ -0,0 +1,21 @@
+# function:
+#make easier code
+# def greet(name):#name is parameter
+#     print("hello",name)
+#     print("welcome to python")
+# greet("mahak")#function call
+# greet("jiya")#jiya is argument
+
+# def say_hello():
+#     print("hello!")
+# say_hello()
+
+def addition(a,b):
+    print("sum",a+b)
+addition(10,20)
+
+def student_name(name,age,grade):
+    print("name:",name)
+    print("age:",age)
+    print("grade:",grade)
+student_name("mahak",20,"BCA")
