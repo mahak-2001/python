a5d706be2616a333ebd932a6eda80767c5b8ed82
mahak-2001 <mahaktanwar171@gmail.com>
2026-07-11 23:55:34 +0530

    test paper solution

---
+++ practicetest1.py
@@ -0,0 +1,145 @@
+#test1
+##which of the following is a valid Python varible name?
+#: 2value  ,  user-name   ,  _user2  ,  class
+
+x=10
+x=str(x)
+print(x*2,type(x).__name__)
+#1010str
+
+value="5.8"
+number=float(value)
+print(int(number),bool(number),type(number).__name__)
+#5 True float
+
+x=10
+def show():
+    x=20
+    print(x,end=" ")
+show()
+print(x)
+#20 10
+
+count=5
+def update():
+    global count
+    count+=3
+update()
+print(count)
+#8
+
+text="Python"
+print(text[1],text[-1],text[1:5:2])
+#y n yh
+
+text=" PyTHOn "
+clean=text.strip()
+print(clean.lower(),clean.upper())
+#python PYTHON
+
+text="banana"
+print(text.replace("a","o",2),text.count("an"))
+#bonona 2
+
+text="red,green,blue"
+parts=text.split(",")
+print("-".join(parts),len(parts))
+# red-green-blue 3
+
+text="intern_python.py"
+print(text.find("python"),text.startswith("intern"),text.endswith(".py"))
+#7 True True
+
+code="Python3"
+year="2026"
+print(code.isalpha(),code.isalnum(),year.isdigit())
+#False True True
+
+numbers=[1,2,3]
+numbers.append([4,5])
+print(numbers)
+#[1,2,3,[4,5]]
+
+numbers=[1,2,3]
+numbers.extend((4,5))
+print(numbers)
+#[1,2,3,4,5]
+
+values=[10,30]
+values.insert(1,20)
+print(values)
+#[10,20,30]
+
+numbers=[1,2,3,2]
+numbers.remove(2)
+print(numbers)
+#[1,3,2]
+
+numbers=[10,20,30]
+value=numbers.pop(1)
+print(value,numbers)
+#20[10,30]
+
+numbers=[3,1,2]
+result=numbers.sort()
+print(result,numbers)
+#none [1,2,3]
+
+values=[1,2,3]
+values.reverse()
+print(values)
+#[3,2,1]
+
+a=[10,20]
+b=a
+c=a.copy()
+b.append(30)
+c.append(40)
+print(a,c)
+#[10,20,30][10,20,40]
+
+result=[x**2 for x in range(1,6)if x%2==0]
+print(result)
+#[4,16]
+
+# data=(10,20,30)
+# data[0]=100
+# #a type error occurs
+
+data=(1,2,[3])
+data[2].append(4)
+print(data)
+#(1,2,[3,4])
+
+data=(1,2)
+extra=[3,4]
+data=data+tuple(extra)
+print(data)
+#(1,2,3,4)
+
+a,b,*c=(1,2,3,4)
+print(a,b,c)
+#1 2 [3,4]
+
+data=(1,2,1,3)
+print(data.count(1),data.index(3))
+#2 3
+
+values={1,1,2,3,3}
+print(len(values))
+#3
+
+values={1,2}
+values.add(3)
+print(len(values),3 in values)
+#3 True
+
+values={1,2}
+values.add((3,4))
+print(len(values),(3,4)in values)
+#3 True
+
+# values={1,2}
+# values.add([3,4])
+# print(values)
+# #TypeError occurs because a list is unhasable
--- test2.py
+++ test2.py
@@ -1,74 +1,77 @@
-# text="I am mahak"
-# count=0
-# for i in text:
-#     if i in ("aeiou,AEIUO"):
-#         count+=1
-#         print(count)
+text="I am mahak"
+count=0
+for i in text:
+    if i in ("aeiou,AEIUO"):
+        count+=1
+        print(count)
 
-# print(text[::-1])
-# word="wow"
-# if (word==word[::-1]):
-#     print("it's a palindrome")
-# else:
-#       print("it's not a palindrome")
+print(text[::-1])
+word="wow"
+if (word==word[::-1]):
+    print("it's a palindrome")
+else:
+      print("it's not a palindrome")
 
-# text="i am mahak. and i am here to learning python."
-# print(text.replace(" ","-"))
-# text1=text.split()
-# print(len(text1))
+text="i am mahak. and i am here to learning python."
+print(text.replace(" ","-"))
+text1=text.split()
+print(len(text1))
 
-# a=[1,2,3,4,5,6,7,8,9,10,1,2,3]
-# c=[11,12]
-# print(sum(a))
-# print(max(a))
-# print(min(a))
-# b=set(a)
-# a=list(b)
-# print(a)
-# d=a+c
-# print(d)
+a=[1,2,3,4,5,6,7,8,9,10,1,2,3]
+c=[11,12]
+print(sum(a))
+print(max(a))
+print(min(a))
+b=set(a)
+a=list(b)
+print(a)
+d=a+c
+print(d)
 
-# x=(1,2,3,7,4,3,1,9,54,2,5,8,9)
-# print(x.count(1))
-# print(max(x))
-# print(min(x))
-# y=list(x)
-# y.append(44)
-# print(y)
+x=(1,2,3,7,4,3,1,9,54,2,5,8,9)
+print(x.count(1))
+print(max(x))
+print(min(x))
+y=list(x)
+y.append(44)
+print(y)
 
-# p={1,2,3,4,5,1,1}
-# q={1,2,3,4,5,6,7,8}
-# print(p.union(q))
-# print(p.intersection(q))
-# print(p.difference(q))
-# print(p.issubset(q))
+p={1,2,3,4,5,1,1}
+q={1,2,3,4,5,6,7,8}
+print(p.union(q))
+print(p.intersection(q))
+print(p.difference(q))
+print(p.issubset(q))
 
 
-# marks={
-#      "math":50,
-#      "hindi":56,
-#      "english":57
-# }
-# mark1={
-#      "sci.":30,
-#      "ss":56,
-#      "eng.":54
-# }
-# # average=sum(marks.values())/len(marks)
-# # print(average,marks)
-# marks.update(mark1)
-# print(marks)
-# name=["mahak","anju","bhumi"]
-# name.sort()
-# print(name)
-# student=[("name","mahak"),("cousre","BCA")]
-# student1=[("name1","bhumi"),("class","12th")]
-# student_list=dict(student)
-# student_list1=dict(student1)
-# print(student_list,student_list1)
-# student_list.update(student_list1)
-# print(student_list)
+marks={
+     "math":50,
+     "hindi":56,
+     "english":57
+}
+mark1={
+     "sci.":30,
+     "ss":56,
+     "eng.":54
+}
+# average=sum(marks.values())/len(marks)
+# print(average,marks)
+marks.update(mark1)
+print(marks)
+name=["mahak","anju","bhumi"]
+name.sort()
+print(name)
+student=[("name","mahak"),("cousre","BCA")]
+student1=[("name1","bhumi"),("class","12th")]
+student_list=dict(student)
+student_list1=dict(student1)
+print(student_list,student_list1)
+student_list.update(student_list1)
+print(student_list)
 text="mahak jiya teena neha mahak"
 words=text.split()
 unique=set(words)
 print(len(unique))
+
+for i in ("mahak"):
+     print(i)
