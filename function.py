e58fae0c0b81c61a7404da024b1aceb2e585bc2d
mahak-2001 <mahaktanwar171@gmail.com>
2026-07-13 19:12:25 +0530

    Update function.py

--- function.py
+++ function.py
@@ -19,3 +19,20 @@ def student_name(name,age,grade):
     print("age:",age)
     print("grade:",grade)
 student_name("mahak",20,"BCA")
+
+#DEFAULT PARAMETER have predefined value.
+#if python
+# def function(a,b,c=10):
+#     print(a,b,c)
+#     function(a=5,6)
+
+
+
+
+
+
+
+def calculate(a,b):
+        return a+b,a-b,a*b
+x,y,z=calculate(3,4)
+print(x,y,z)
