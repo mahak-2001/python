7c5f6abccb1d12972b346880fb78e3f7770d33fe
mahak-2001 <mahaktanwar171@gmail.com>
2026-07-16 23:16:03 +0530

    changes

--- function questions.py
+++ function questions.py
@@ -39,202 +39,219 @@ def build_record(**data):
      return sorted(data.items())
 print(build_record(a=2,b=3,name="X"))
 
-def calculate(a,b,c):
-     return a+b*c
-values=(2,3)
-options={"c":4}
-print(calculate(*values,**options))
+# def calculate(a,b,c):
+#      return a+b*c
+# values=(2,3)
+# options={"c":4}
+# print(calculate(*values,**options))
 
-def report(name,*,score=0,passed=True):
-     return f"{name}:{score}:{passed}"
-print(report("Riya",score=88))
-print(report("kabir",passed=False,score=40))
+# def report(name,*,score=0,passed=True):
+#      return f"{name}:{score}:{passed}"
+# print(report("Riya",score=88))
+# print(report("kabir",passed=False,score=40))
 
-def change(number): 
-    number += 10 
-    return number 
+# def change(number): 
+#     number += 10 
+#     return number 
  
-value = 5 
+# value = 5 
  
-print(change(value), value) 
+# print(change(value), value) 
 
 
-def update(data): 
-    data[0] += 5 
-    data.append(sum(data)) 
+# def update(data): 
+#     data[0] += 5 
+#     data.append(sum(data)) 
  
-numbers = [1, 2, 3] 
+# numbers = [1, 2, 3] 
  
-update(numbers) 
-print(numbers) 
+# update(numbers) 
+# print(numbers) 
  
  
-def update(data): 
-    data = data + [4] 
-    data[0] = 99 
-    return data 
+# def update(data): 
+#     data = data + [4] 
+#     data[0] = 99 
+#     return data 
  
-numbers = [1, 2, 3] 
-new_numbers = update(numbers) 
+# numbers = [1, 2, 3] 
+# new_numbers = update(numbers) 
  
-print(numbers) 
-print(new_numbers) 
- 
-
-def update(record): 
-    record["b"] = record.get("b", 0) + 2 
-    record = {"c": 3} 
-    return record 
- 
-data = {"a": 1, "b": 4} 
-result = update(data) 
- 
-print(data) 
-print(result) 
+# print(numbers) 
+# print(new_numbers) 
  
 
-def modify(data): 
-    data[1].append(30) 
-    return data + ("done",) 
+# def update(record): 
+#     record["b"] = record.get("b", 0) + 2 
+#     record = {"c": 3} 
+#     return record 
  
-values = (10, [20]) 
-result = modify(values) 
+# data = {"a": 1, "b": 4} 
+# result = update(data) 
+ 
+# print(data) 
+# print(result) 
+ 
 
-print(values) 
-print(result) 
+# def modify(data): 
+#     data[1].append(30) 
+#     return data + ("done",) 
+ 
+# values = (10, [20]) 
+# result = modify(values) 
+
+# print(values) 
+# print(result) 
   
-x = 10 
+# x = 10 
  
-def outer(): 
-    x = 20 
+# def outer(): 
+#     x = 20 
  
-    def inner(): 
-        global x 
-        x += 5 
-        print(x) 
+#     def inner(): 
+#         global x 
+#         x += 5 
+#         print(x) 
  
-    inner() 
-    print(x) 
+#     inner() 
+#     print(x) 
  
-outer() 
-print(x) 
- 
-
-def outer(): 
-    number = 1 
- 
-    def inner(): 
-        nonlocal number 
-        number *= 3 
-        return number 
- 
-    print(inner(), inner()) 
- 
-outer() 
+# outer() 
+# print(x) 
  
 
-def create_power(exponent): 
-    def calculate(number): 
-        return number ** exponent 
+# def outer(): 
+#     number = 1 
  
-    return calculate 
+#     def inner(): 
+#         nonlocal number 
+#         number *= 3 
+#         return number 
  
-square = create_power(2) 
-cube = create_power(3) 
+#     print(inner(), inner()) 
  
-print(square(4) + cube(2)) 
+# outer() 
  
 
-functions = [] 
+# def create_power(exponent): 
+#     def calculate(number): 
+#         return number ** exponent 
  
-for number in range(3): 
-    functions.append(lambda: number) 
+#     return calculate 
  
-print([function() for function in functions]) 
+# square = create_power(2) 
+# cube = create_power(3) 
+ 
+# print(square(4) + cube(2)) 
+ 
+
+# functions = [] 
+ 
+# for number in range(3): 
+#     functions.append(lambda: number) 
+ 
+# print([function() for function in functions]) 
  
  
-functions = [] 
+# functions = [] 
  
-for number in range(3): 
-    functions.append(lambda number=number: number) 
+# for number in range(3): 
+#     functions.append(lambda number=number: number) 
  
-print([function() for function in functions]) 
+# print([function() for function in functions]) 
   
 
-def apply(function, values): 
-    return [ 
-        function(value) 
-        for value in values 
-        if function(value) > 5 
-    ] 
+# def apply(function, values): 
+#     return [ 
+#         function(value) 
+#         for value in values 
+#         if function(value) > 5 
+#     ] 
 
-def transform(number): 
-    return number * number - 1 
+# def transform(number): 
+#     return number * number - 1 
  
-print(apply(transform, [1, 2, 3, 4])) 
- 
-
-numbers = [1, 2, 3, 4, 5] 
- 
-result = list( 
-    map( 
-        lambda number: number * 2, 
-        filter(lambda number: number % 2 == 1, numbers) 
-    ) 
-) 
-print(result) 
+# print(apply(transform, [1, 2, 3, 4])) 
  
 
-def add(a, b): 
-    return a + b 
+# numbers = [1, 2, 3, 4, 5] 
  
-def multiply(a, b): 
-    return a * b 
+# result = list( 
+#     map( 
+#         lambda number: number * 2, 
+#         filter(lambda number: number % 2 == 1, numbers) 
+#     ) 
+# ) 
+# print(result) 
  
-operations = { 
-    "addition": add, 
-    "multiplication": multiply 
-} 
+
+# def add(a, b): 
+#     return a + b 
  
-result = ( 
-    operations["addition"](2, 3) 
-    + operations["multiplication"](2, 3) 
-) 
+# def multiply(a, b): 
+#     return a * b 
  
-print(result) 
+# operations = { 
+#     "addition": add, 
+#     "multiplication": multiply 
+# } 
+ 
+# result = ( 
+#     operations["addition"](2, 3) 
+#     + operations["multiplication"](2, 3) 
+# ) 
+ 
+# print(result) 
  
  
 
-def calculate(number): 
-    if number <= 1: 
-        return 1 
+# def calculate(number): 
+#     if number <= 1: 
+#         return 1 
  
-    return number + calculate(number - 2) 
+#     return number + calculate(number - 2) 
  
-print(calculate(6)) 
+# print(calculate(6)) 
  
 
-def trace(number): 
-    if number == 0: 
-        return 
+# def trace(number): 
+#     if number == 0: 
+#         return 
  
-    print(number, end=" ") 
-    trace(number - 1) 
-    print(number, end=" ") 
+#     print(number, end=" ") 
+#     trace(number - 1) 
+#     print(number, end=" ") 
  
-trace(3) 
+# trace(3) 
  
  
-def flatten(data): 
-    result = [] 
+# def flatten(data): 
+#     result = [] 
  
-    for item in data: 
-        if isinstance(item, list): 
-            result.extend(flatten(item)) 
-        else: 
-            result.append(item) 
+#     for item in data: 
+#         if isinstance(item, list): 
+#             result.extend(flatten(item)) 
+#         else: 
+#             result.append(item) 
  
-    return result 
+#     return result 
  
-values = [1, [2, [3, 4]], 5] 
-print(flatten(values))
+# values = [1, [2, [3, 4]], 5] 
+# print(flatten(values))
+
+
+
+
+
+
+# functions=[]
+# for number in range(3):
+#     functions.append(lambda:number)
+# print([function()for function in functions])
+
+
+
+
+
+
+
