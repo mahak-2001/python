2f99d04b90a604f8a313e047f55a6a6d721783a3
mahak-2001 <mahaktanwar171@gmail.com>
2026-07-20 12:47:55 +0530

    Update filehandling.py

--- filehandling.py
+++ filehandling.py
@@ -1,12 +1,12 @@
 
-# # #absolute and manually path
-# # # mode,path,encoding
-# # # file=open("C:\Users\Monika\OneDrive\Documents\GitHub\python\python\loop.py","r",encoding="utf-8")
+# #absolute and manually path
+# # mode,path,encoding
+# # file=open("C:\Users\Monika\OneDrive\Documents\GitHub\python\python\loop.py","r",encoding="utf-8")
 
-# file=open("test.py","r",encoding="utf-8")
-# print(file.read())
-# file.close()
-# print(file.closed)
+file=open("test.py","r",encoding="utf-8")
+print(file.read())
+file.close()
+print(file.closed)
 
 with open("set.py","r",encoding="utf-8") as file:
     print(file.read())
